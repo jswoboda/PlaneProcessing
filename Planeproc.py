3d13eb4218fe3fc3aa21321a640fa8878f7cdc8f
John Swoboda <swoboj@bu.edu>
2015-12-30 16:23:59 -0500

    removed plotted to use elsewhere

--- Planeproc.py
+++ Planeproc.py
@@ -11,12 +11,11 @@ import pdb
 import scipy as sp
 import matplotlib
 matplotlib.use('Agg')
-from RadarDataSim.IonoContainer import IonoContainer, MakeTestIonoclass
+from RadarDataSim.IonoContainer import MakeTestIonoclass
 import RadarDataSim.runsim as runsim
 from RadarDataSim.analysisplots import analysisdump
 from RadarDataSim.utilFunctions import readconfigfile
-import matplotlib.pyplot as plt
-from GeoData.GeoData import GeoData
+
 from  GeoData.utilityfuncs import readIono
 
 
@@ -141,132 +140,6 @@ def ke(item):
     else:
         return float('inf')
 #%%
-def plotoutdata(testdir,imgdir):
-    """This will plot all of the input data with each time step as a pcolor images of
-    electron density and ion tempreture.
-    Inputs
-    testdir - The directory with the input data in h5 files formated for the ionocontainer structure.
-    imgdir - The directory that holds the images."""
-
-    if os.path.exists(imgdir):
-        imgfiles = glob.glob(os.path.join(imgdir,'*.png'))
-        for imgf in imgfiles:
-            os.remove(imgf)
-    else:
-        os.mkdir(imgdir)
-
-    filelist = glob.glob(os.path.join(testdir,'Origparams','*.h5'))
-    numlist = [os.path.splitext(os.path.split(x)[-1])[0] for x in filelist]
-    numdict = {numlist[i]:filelist[i] for i in range(len(filelist))}
-    slist = sorted(numlist,key=ke)
-
-    imcount = 0
-    filetemplate = 'outputdata'
-
-
-    for inum in slist:
-        ifile = numdict[inum]
-        iono = IonoContainer.readh5(ifile)
-        Iono1 = GeoData(readIono,[iono])
-        nt = Iono1.times.shape[0]
-        rng = sp.sqrt(Iono1.dataloc[:,0]**2+Iono1.dataloc[:,1]**2)
-        z = Iono1.dataloc[:,2]
-        rngvec = sp.unique(rng)
-        zvec = sp.unique(z)
-        rngmat = rng.reshape(len(zvec),len(rngvec))
-        zmat = z.reshape(len(zvec),len(rngvec))
-        Ne = Iono1.data['Ne'].reshape(len(zvec),len(rngvec),nt)
-        Ti = Iono1.data['Ti'].reshape(len(zvec),len(rngvec),nt)
-
-        for itimen,itime in enumerate(Iono1.times):
-            fig = plt.figure(facecolor='w',figsize=(14, 8))
-            ax1=fig.add_subplot(1,2,1)
-            ax2=fig.add_subplot(1,2,2)
-
-            ax1.set_title('Ne')
-            ax2.set_title('Ti')
-            ax1.set_xlabel('Range in km')
-            ax1.set_ylabel('Alt in km')
-            ax2.set_xlabel('Range in km')
-            ax2.set_ylabel('Alt in km')
-            pc1 = ax1.pcolor(rngmat,zmat,Ne[:,:,itimen],cmap = 'jet',vmin=5e10,vmax=2e11)
-
-            pc2 = ax2.pcolor(rngmat,zmat,Ti[:,:,itimen],cmap = 'jet',vmin=3000,vmax=6000)
-            ax1.set_xlim([rngmat.min(),rngmat.max()])
-            ax2.set_ylim([zmat.min(),zmat.max()])
-            spti = fig.suptitle('Parameters at {0} seconds'.format(int(itime[0])))
-
-            cb1 = plt.colorbar(pc1, ax=ax1,format='%.0e')
-            cb2 = plt.colorbar(pc2, ax=ax2,format='%.0e')
-
-
-            fname= '{0:0>3}_'.format(imcount)+filetemplate+'.png'
-            plt.savefig(os.path.join(imgdir,fname))
-            imcount=imcount+1
-            plt.close(fig)
-
-def plotoutput(testdir,imgdir):
-    """This will plot all of the fitted data with each time step as a pcolor images of
-    electron density and electron density from power mesurements.
-    Inputs
-    testdir - The directory with the input data in h5 files formated for the ionocontainer structure.
-    imgdir - The directory that holds the images."""
-    if os.path.exists(imgdir):
-        imgfiles = glob.glob(os.path.join(imgdir,'*.png'))
-        for imgf in imgfiles:
-            os.remove(imgf)
-    else:
-        os.mkdir(imgdir)
-    filename = os.path.join(testdir,'Fitted','fitteddata.h5')
-    iono = IonoContainer.readh5(filename)
-    Iono1 = GeoData(readIono,[iono])
-
-    rngrdr =Iono1.dataloc[:,0]
-    el = Iono1.dataloc[:,2]
-    rngrdrvec,rndinv = sp.unique(rngrdr,return_inverse=True)
-    elvec,elinv = sp.unique(el,return_inverse=True)
-    nt = Iono1.times.shape[0]
-    Rngrdrmat = sp.reshape(rngrdr,(len(rngrdrvec),len(elvec)))
-    Elmat = sp.reshape(el,(len(rngrdrvec),len(elvec)))
-
-
-    Xmat = Rngrdrmat*sp.cos(Elmat*sp.pi/180.)
-    Zmat = Rngrdrmat*sp.sin(Elmat*sp.pi/180.)
-    Ne = Iono1.data['Ne'].reshape(len(rngrdrvec),len(elvec),nt)
-    Ti = Iono1.data['Nepow'].reshape(len(rngrdrvec),len(elvec),nt)
-
-
-    imcount=0
-    filetemplate = 'fitteddata'
-
-    for itimen,itime in enumerate(Iono1.times):
-        fig = plt.figure(facecolor='w',figsize=(14, 8))
-        ax1=fig.add_subplot(1,2,1)
-        ax2=fig.add_subplot(1,2,2)
-
-        ax1.set_title('Ne')
-        ax2.set_title('Ne From Power')
-        ax1.set_xlabel('Range in km')
-        ax1.set_ylabel('Alt in km')
-        ax2.set_xlabel('Range in km')
-        ax2.set_ylabel('Alt in km')
-        Nemat = Ne[:,:,itimen]
-        Timat = Ti[:,:,itimen]
-        pc1 = ax1.pcolor(Xmat,Zmat,Nemat,cmap = 'jet',vmin=5e10,vmax=2e11)
-
-        pc2 = ax2.pcolor(Xmat,Zmat,Timat,cmap = 'jet',vmin=5e10,vmax=5e11)
-        ax1.set_xlim([Xmat.min(),Xmat.max()])
-        ax2.set_ylim([Zmat.min(),Zmat.max()])
-        spti = fig.suptitle('Parameters at {0} seconds'.format(int(itime[0])))
-#            if imcount==0:
-        cb1 = plt.colorbar(pc1, ax=ax1,format='%.0e')
-        cb2 = plt.colorbar(pc2, ax=ax2,format='%.0e')
-#            ims.append([pc1,pc2])
-
-        fname= '{0:0>3}_'.format(imcount)+filetemplate+'.png'
-        plt.savefig(os.path.join(imgdir,fname))
-        imcount=imcount+1
-        plt.close(fig)
 
 def runradarsims(testpath,funcnamelist=['spectrums','radardata','fitting'],configfile = 'planeproc2.ini',remakealldata=False):
     """ This will run the radar simulations for all the selected data sets"""
