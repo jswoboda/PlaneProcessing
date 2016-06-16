cb7f6853e34049e312f1147ff644f77cc5a52837
John Swoboda <swoboj@bu.edu>
2016-06-16 19:23:58 -0400

    fixed small isue with the stationary bash file

--- runplanestat.sh
+++ runplanestat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python PlaneProc.py -f radardata fitting plotting -i ~/DATA/PlaneProcessing/Phased_Array/exp_width_stat_01 -c ~/DATA/PlaneProcessing/Phased_Array/planeproc2_stat.ini -r y
-cp ~/DATA/PlaneProcessing/Phased_Array/exp_width_01_stat/fittedimages/*.png  ~/Dropbox/PlaneProcessing/Stationary/fittedimages/
+cp ~/DATA/PlaneProcessing/Phased_Array/exp_width_stat_01/fittedimages/*.png  ~/Dropbox/PlaneProcessing/Stationary/fittedimages/
--- runplanestat2.sh
+++ runplanestat2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python PlaneProc.py -f radardata fitting plotting -i ~/DATA/PlaneProcessing/Phased_Array/exp_width_state_01_red -c ~/DATA/PlaneProcessing/Phased_Array/planeproc2_stat_red.ini -r y
-cp ~/DATA/PlaneProcessing/Phased_Array/exp_width_01/fittedimages/*.png  ~/Dropbox/PlaneProcessing/Stationary_Reduced/fittedimages/
+python PlaneProc.py -f radardata fitting plotting -i ~/DATA/PlaneProcessing/Phased_Array/exp_width_stat_01_red -c ~/DATA/PlaneProcessing/Phased_Array/planeproc2_stat_red.ini -r y
+cp ~/DATA/PlaneProcessing/Phased_Array/exp_width_stat_01_red/fittedimages/*.png  ~/Dropbox/PlaneProcessing/Stationary_Reduced/fittedimages/
