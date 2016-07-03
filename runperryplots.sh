0d26a85c1f68ce987cbeee7cb341fc617daf2a9d
John Swoboda <swoboj@bu.edu>
2016-07-03 15:15:05 -0400

    updates to bash scripts

--- runperryplots.sh
+++ runperryplots.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python PlaneProc.py -f  fitting plotting -i ~/DATA/PerryPlane/MattsData/ -c ~/DATA/PerryPlane/planeproc2.ini -r True
+python PlaneProc.py -f  radardata fitting plotting -i ~/DATA/PerryPlane/MattsData/ -c ~/DATA/PerryPlane/planeproc2.ini -r True
 cp ~/DATA/PerryPlane/MattsData/fittedimages/*.png  ~/Dropbox/PerryPlane/fittedimages60seconds/
 python PlaneProcUtil.py -f ~/Dropbox/PerryPlane/fittedimages60seconds/ -p ~/Dropbox/PerryPlane/fittedimages60seconds2/ -i 60
