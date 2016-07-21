5491df0e6062058b70c9c23c7e0e0bee7e36be2e
John Swoboda <swoboj@bu.edu>
2016-07-21 11:41:29 -0400

    updated bash scripts

--- runperryplotsred.sh
+++ runperryplotsred.sh
@@ -2,4 +2,4 @@
 
 python PlaneProc.py -f  spectrums radardata fitting plotting -i ~/DATA/PerryPlane/MattsDataRed/ -c ~/DATA/PerryPlane/planeproc2red.ini  -r True
 cp ~/DATA/PerryPlane/MattsDataRed/fittedimages/*.png  ~/Dropbox/PerryPlane/fittedimages60secondsRed/
-python PlaneProcUtil.py -f ~/Dropbox/PerryPlane/fittedimages60secondsRed/ -p ~/Dropbox/PerryPlane/fittedimages60secondsRed2/ -i 300
+python PlaneProcUtil.py -f ~/Dropbox/PerryPlane/fittedimages60secondsRed/ -p ~/Dropbox/PerryPlane/fittedimages60secondsRed2/ -i 420
