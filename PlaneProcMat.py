eb68808cc7159bfa4863c94eadefb108beac8077
John Swoboda <swoboj@bu.edu>
2016-10-13 08:41:32 -0400

    change bounds of l curves

--- PlaneProcMat.py
+++ PlaneProcMat.py
@@ -229,7 +229,7 @@ def parametersweep(basedir,configfile,acfdir='ACF',invtype='tik'):
         invtype - The inversion method that will be tested. Can be tik, tikd, and tv.
         """
 
-    alpha_sweep=sp.logspace(-2,sp.log10(2.)/2,5)
+    alpha_sweep=sp.logspace(-4,0,5)
     costdir = os.path.join(basedir,'Cost')
     ionoinfname=os.path.join(basedir,acfdir,'00lags.h5')
     ionoin=IonoContainer.readh5(ionoinfname)
